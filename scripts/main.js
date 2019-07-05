ac62f4c2ee35f414d2b60327873f0117bf141bac
mohammad jamil rezaei <mjrafg@yahoo.com>
2019-07-06 00:10:56 +0430

    firstupload by mohammad jamil rezaei

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  const width = $(".main-content-center").first().width()
+  $(".main-content-center").first().height(width);
+  $(".main-item").on({
+    mouseenter: function (elem) {
+      var mainItems = $('.main-item');
+      mainItems.map((index, element) => {
+        if (element != elem.currentTarget) {
+          $(element).css('opacity', .5)
+        }
+      });
+    },
+    mouseleave: function () {
+      $(".main-item").css('opacity', 1)
+
+    }
+  })
+});
